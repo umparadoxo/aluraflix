a12528a36db3fb9ee899353aa4e1d5557342b24d
Matheus <legiaored@gmail.com>
2020-08-01 19:09:29 -0300

    Adicionada a Page Default e o Roteamento

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Logo from '../../assets/Logo.png';
 import './Menu.css';
 import Button from '../Button';
@@ -9,15 +10,15 @@ function Menu() {
     return (
 
         <nav className='Menu'>
-            <a href='/'>
+            <Link to='/'>
                 <img className='Logo' src={Logo} alt='Logo da Aluraflix'></img>
-            </a>
+            </Link>
 
-            <Button as ='a' className='ButtonLink' href='/'>
+            <Button as = {Link} className='ButtonLink' to='/cadastro/video'>
                 Novo vídeo
             </Button>
 
-        </nav>
+        </nav>  
 
 
 
